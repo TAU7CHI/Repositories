2d3aa5a13a244706bdd0ac658cb3e27ad4a45b12
TAU7CHI <114913104+TAU7CHI@users.noreply.github.com>
2023-08-25 11:31:17 +0200

    Hotel page

---
+++ Hotel/script.js
@@ -0,0 +1,48 @@
+
+var swiper = new Swiper(".home-slider",{
+     grabCursor: true,
+     loop: true,
+     centeredSlides: true,
+      autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+      },
+
+      navigation:{
+
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-next",
+      }
+});
+
+
+var swiper = new Swiper(".room-slider",{
+
+  spaceBetween:20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+   autoplay: {
+     delay: 7500,
+     disableOnInteraction: false,
+   },
+
+  pagination: {
+
+    el:".swiper-pagination",
+    clickable:true,
+  },
+
+  breakpoints: {
+    0: {
+
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    991:{
+      slidesPerView: 3, 
+    },
+  },
+});
